e66536146d395e74bc5461f93c0a0b3ebe379b9e
RatSauce <58752704+SpicyRatSauce@users.noreply.github.com>
2023-12-29 19:31:03 -0400

    Update InstallSoftware.sh
    Updated method of installing flatpaks

--- InstallSoftware.sh
+++ InstallSoftware.sh
@@ -1,10 +1,41 @@
 #!/bin/bash
 
-echo "Starting software install!"
+# List of Flatpak application IDs
+flatpak_apps=(
+    org.mozilla.firefox
+    com.google.Chrome
+    com.github.tchx84.Flatseal
+    it.mijorus.gearlever
+    com.github.Matoking.protontricks
+    net.davidotek.pupgui2
+    org.prismlauncher.PrismLauncher
+    com.heroicgameslauncher.hgl
+    net.lutris.Lutris
+    com.usebottles.bottles
+    com.discordapp.Discord
+    com.spotify.Client
+    org.kde.krita
+    org.darktable.Darktable
+    # Add more application IDs as needed
+)
 
-# Install useful software using flatpak
-flatpak install -y --noninteractive flathub org.mozilla.firefox com.google.Chrome com.github.tchx84.Flatseal it.mijorus.gearlever com.github.Matoking.protontricks net.davidotek.pupgui2 org.prismlauncher.PrismLauncher com.heroicgameslauncher.hgl net.lutris.Lutris com.usebottles.bottles com.discordapp.Discord com.spotify.Client org.kde.krita org.darktable.Darktable
+# Function to install Flatpak applications
+install_flatpak_apps() {
+    for app_id in "${flatpak_apps[@]}"; do
+        flatpak install -y $app_id
+        if [ $? -eq 0 ]; then
+            echo "Successfully installed: $app_id"
+        else
+            echo "Failed to install: $app_id"
+        fi
+    done
+}
 
-wait
+# Check if Flatpak is installed
+if ! command -v flatpak &> /dev/null; then
+    echo "Flatpak is not installed. Please install Flatpak first."
+    exit 1
+fi
 
-echo "Software installed!"
+# Install Flatpak applications
+install_flatpak_apps

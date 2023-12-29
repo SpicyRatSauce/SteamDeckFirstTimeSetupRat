7ab5d723e51da178ee311fcbba6f122bf939be01
RatSauce <58752704+SpicyRatSauce@users.noreply.github.com>
2023-12-29 19:27:11 -0400

    Update SteamOS_Setup.sh

--- SteamOS_Setup.sh
+++ SteamOS_Setup.sh
@@ -16,17 +16,22 @@ done
 # Download And install Cryoutils
 echo "Downloading & running CryoUtils installer!"
 curl https://raw.githubusercontent.com/CryoByte33/steam-deck-utilities/main/install.sh | bash -s --
+wait $!
 echo "CryoUtils installed!"
+
 
 
 # Download and install Decky Loader
 echo "Downloading & running Decky Loader installer!"
 curl https://raw.githubusercontent.com/SteamDeckHomebrew/decky-loader/e3d72b60823014317fe56a51cbfffd4bb7f33453/dist/install_release.sh | bash -s --
+wait $!
 echo "Decky Loader installed!"
+
 
 
 # Download and run software install script
 echo "Installing Software!"
 curl https://raw.githubusercontent.com/SpicyRatSauce/SteamDeckFirstTimeSetupRat/1e1b90f2b3af44c64f9c03f396e41a773b035c84/InstallSoftware.sh | bash -s --
+wait $!
 echo "Software Installed! Setup finished!"
 

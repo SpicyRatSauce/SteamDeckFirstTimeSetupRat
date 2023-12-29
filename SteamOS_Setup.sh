2cbf6aad88e9d868eb0b7cf0cb10c47b78f6317a
RatSauce <58752704+SpicyRatSauce@users.noreply.github.com>
2023-12-29 19:14:37 -0400

    Update SteamOS_Setup.sh

--- SteamOS_Setup.sh
+++ SteamOS_Setup.sh
@@ -38,12 +38,6 @@ sudo systemctl restart NetworkManager
 echo "Wifi power management disabled"
 
 
-#echo "Starting software install!"
-
-# Install useful software using flatpak
-#flatpak install flathub org.mozilla.firefox com.google.Chrome com.github.tchx84.Flatseal it.mijorus.gearlever com.github.Matoking.protontricks net.davidotek.pupgui2 org.prismlauncher.PrismLauncher com.heroicgameslauncher.hgl net.lutris.Lutris com.usebottles.bottles com.discordapp.Discord com.spotify.Client org.kde.krita org.darktable.Darktable -y  
-#wait
-#echo "Software installed!"
 
 
 

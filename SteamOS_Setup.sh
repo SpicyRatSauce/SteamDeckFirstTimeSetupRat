a6fd8017cf376b5c12c2c7ceb7f8c1fd8f380017
RatSauce <58752704+SpicyRatSauce@users.noreply.github.com>
2023-12-29 19:45:36 -0400

    Update SteamOS_Setup.sh

--- SteamOS_Setup.sh
+++ SteamOS_Setup.sh
@@ -35,3 +35,16 @@ curl https://raw.githubusercontent.com/SpicyRatSauce/SteamDeckFirstTimeSetupRat/
 wait $!
 echo "Software Installed! Setup finished!"
 
+# Disable readonly file system
+sudo steamos-readonly disable
+
+# Disable wifi power management
+if test -f /etc/NetworkManager/conf.d/default-wifi-powersave-on.conf; then
+  sudo sed -i 's/3/2/' /etc/NetworkManager/conf.d/default-wifi-powersave-on.conf
+else
+  sudo echo -e "[connection]\nwifi.powersave = 3" >> /etc/NetworkManager/conf.d/default-wifi-powersave-on.conf
+
+sudo systemctl restart NetworkManager
+
+echo "Wifi power management disabled"
+
